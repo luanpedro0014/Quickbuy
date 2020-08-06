73edb98ca4969c6e140570f2b952a11e514aa66c
Luan Pedro <luanpedro0014@gmail.com>
2020-08-06 11:11:08 -0300

    Criação das Entidades

---
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,33 @@
+﻿using QuickBuy.Dominio.ObjetosdeValor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades
+{
+   public class Pedido
+    {
+        public int Id { get; set; }
+
+        public DateTime DataPedido { get; set; }
+        public int UsuarioID { get; set; }
+
+        public ICollection<ItemPedido> ItensPedido  { get; set; }  //Pedido deve  ter pelo menos um Item dpedido ou muitos Itens de pedidos.
+
+        public DateTime DataPrevisaoEntrega { get; set; }
+
+        public string CEP { get; set; }
+        public string Estado { get; set; }
+
+        public string 
+           Cidade{ get; set; }
+
+        public int EnderecoCompleto { get; set; }
+
+        public int FormaPagamentoId { get; set; }
+
+        public FormaPagamento FormaPagamento { get; set; }
+
+
+    }
+}
---
+++ QuickBuy.Dominio/ObjetosdeValor/FormaPagamento.cs
@@ -0,0 +1,34 @@
+﻿using QuickBuy.Dominio.Enumerados;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.ObjetosdeValor
+{
+   public  class FormaPagamento
+    {
+        public int Id { get; set; }
+
+        public int Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public bool EhBoleto {
+            get { return Id == (int)TiposFormaPagamentoEnum.Boleto; }
+       
+        }
+        public bool EhCartaoCredito
+        {
+            get { return Id == (int)TiposFormaPagamentoEnum.CartaoCredito; }
+
+        }
+
+        public bool NãofoiDefinido
+        {
+            get { return Id == (int)TiposFormaPagamentoEnum.NaoDefinido; }
+
+        }
+        
+
+    }
+}
